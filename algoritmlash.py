fafc932ddc1f3c508e23e6cab264890204cf608b
Temur Xasanov <temurxasanov320@gmail.com>
2026-07-29 11:44:05 +0500

    two pointer

--- algoritmlash.py
+++ algoritmlash.py
@@ -14,10 +14,29 @@ def returnindex(nums:list, target:int):
         
 
 def zero(nums:list):
-    for i in range(0, len(nums)):
-        if nums[i] == 0:
-            nums.append(nums.pop(i))
+    position = 0
+    for number in nums:
+        if number!=0:
+            nums[position] = number
+            position +=1
+    while position<len(nums):
+        nums[position] = 0
+        position+=1
     return nums
+
+
+def sumtagret(nums:list, target:int):
+    i = 0
+    result = []
+    while i<len(nums)-1:
+        j = i+1
+        while j<len(nums):
+            if nums[j]+nums[i] == target:
+                result.append([nums[i],nums[j]])
+            j+=1
+        i+=1
+    return result
+
 a = list(map(int,input("sonlarni kirit: ").split()))
-# b = int(input("Son kirit: "))
-print(zero(a))
+b = int(input("Son kirit: "))
+print(sumtagret(a,b))
